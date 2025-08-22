59deb419e066794c5c9e285bfe978afc0d9ef403
Harold Revueltas <haroldrevueltas12@hotmail.com>
2025-08-22 11:26:53 -0500

    Remove unused import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { getCssText } from "@/stitches.config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
